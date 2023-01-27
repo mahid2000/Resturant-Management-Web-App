b6d2690074a457647bc4e0c3985c98163aeee9d5
Mahid Gondal <ZJAC351@live.rhul.ac.uk>
2023-01-27 02:38:52 +0000

    feat[schema]: added schema for transaction tables

--- schema.sql
+++ schema.sql
@@ -1,4 +1,8 @@
 DROP TABLE IF EXISTS menu;
+-- DROP TABLE IF EXISTS customer;
+-- DROP TABLE IF EXISTS transaction;
+-- DROP TABLE IF EXISTS orderedItem;
+
 
 CREATE TABLE menu (
 	itemID SERIAL PRIMARY KEY,
@@ -7,3 +11,30 @@ CREATE TABLE menu (
 	calories INTEGER,
 	allergens VARCHAR(256)
 );
+
+
+-- These are all schemas for the ARMS system that have yet to be tested
+
+-- CREATE TABLE customer (
+-- 	customerID SERIAL PRIMARY KEY,
+-- 	name VARCHAR(256)
+-- );
+--
+-- CREATE TABLE transaction (
+-- 	transactionID SERIAL PRIMARY KEY,
+-- 	customerID SERIAL PRIMARY KEY,
+--     tableNumber INTEGER,
+--     total INTEGER
+-- );
+--
+-- CREATE TABLE orderedItem (
+-- 	orderedItemID SERIAL PRIMARY KEY,
+-- 	ItemID SERIAL PRIMARY KEY,
+-- 	orderID SERIAL PRIMARY KEY,
+-- 	name VARCHAR(256),
+-- 	price INTEGER,
+-- 	calories INTEGER,
+-- 	allergens VARCHAR(256)
+-- );
+
+
