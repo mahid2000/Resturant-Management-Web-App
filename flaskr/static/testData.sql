c314a8ddffcd9761ccb73a40147530b4853d2290
Mahmud-Nuh-Abdirahman <zkac448@live.rhul.ac.uk>
2023-03-22 16:40:57 +0000

    Added image_location to testData

--- flaskr/static/testData.sql
+++ flaskr/static/testData.sql
@@ -1,37 +1,37 @@
 -- Menu Table
 
 -- Mains
-INSERT INTO menu (name, price, category, calories, allergens)
+INSERT INTO menu (name, price, category, calories, allergens, image_location)
 VALUES
-    ("Beef Tacos", 12.50, "Mains", 800, "Mi"),
-    ("Pork Tacos", 12.50, "Mains", 800, "Mi"),
-    ("Halloumi Tacos", 10.50, "Mains", 750, "Mi"),
-    ("Chicken Enchiladas", 14.50, "Mains", 900, "G"),
-    ("Pork Enchiladas", 14.50, "Mains", 900, "Mi"),
-    ("Jackfruit Enchiladas", 14.50, "Mains", 900, "Mi");
+    ("Beef Tacos", 12.50, "Mains", 800, "Mi",""),
+    ("Pork Tacos", 12.50, "Mains", 800, "Mi",""),
+    ("Halloumi Tacos", 10.50, "Mains", 750, "Mi",""),
+    ("Chicken Enchiladas", 14.50, "Mains", 900, "G",""),
+    ("Pork Enchiladas", 14.50, "Mains", 900, "Mi",""),
+    ("Jackfruit Enchiladas", 14.50, "Mains", 900, "Mi","");
 
 -- Sides
-INSERT INTO menu (name, price, category, calories, allergens)
+INSERT INTO menu (name, price, category, calories, allergens, image_location)
 VALUES
-    ("Plain Nachos", 5.50, "Sides", 350, ""),
-    ("Cheesy Nachos", 6.50, "Sides", 400, "Mi");
+    ("Plain Nachos", 5.50, "Sides", 350, "",""),
+    ("Cheesy Nachos", 6.50, "Sides", 400, "Mi","");
 
 -- Desserts
-INSERT INTO menu (name, price, category, calories, allergens)
+INSERT INTO menu (name, price, category, calories, allergens, image_location)
 VALUES
-    ("Ice Cream Sundae", 5.50, "Desserts", 300, "Mi"),
-    ("Chocolate Brownie", 5.50, "Desserts", 300, "Mi");
+    ("Ice Cream Sundae", 5.50, "Desserts", 300, "Mi",""),
+    ("Chocolate Brownie", 5.50, "Desserts", 300, "Mi","");
 
 -- Drinks
-INSERT INTO menu (name, price, category, calories, allergens)
+INSERT INTO menu (name, price, category, calories, allergens, image_location)
 VALUES
-    ("Coca Cola", 3.50, "Drinks", 150, ""),
-    ("Fanta", 3.50, "Drinks", 150, ""),
-    ("Sprite", 3.50, "Drinks", 150, ""),
-    ("Apple Juice", 3.50, "Drinks", 150, ""),
-    ("Orange Juice", 3.50, "Drinks", 150, ""),
-    ("Still Water", 3.50, "Drinks", 150, ""),
-    ("Beer", 3.50, "Drinks", 150, "G");
+    ("Coca Cola", 3.50, "Drinks", 150, "",""),
+    ("Fanta", 3.50, "Drinks", 150, "",""),
+    ("Sprite", 3.50, "Drinks", 150, "",""),
+    ("Apple Juice", 3.50, "Drinks", 150, "",""),
+    ("Orange Juice", 3.50, "Drinks", 150, "",""),
+    ("Still Water", 3.50, "Drinks", 150, "",""),
+    ("Beer", 3.50, "Drinks", 150, "G","");
 
 
 -- Logins
